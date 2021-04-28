96595b52934dc5ccddad3f8ac9b5c05cfa509a2f
andrea <andrea@debian.com>
2021-04-28 16:11:50 +0200

    Update authorization_log.py and personality notebook
    Fix some problems with the management of jwt

--- authorization_log.py
+++ authorization_log.py
@@ -10,6 +10,9 @@ import crypto.keyspb.keyspb_pb2
 from google.protobuf import duration_pb2
 import trillian_log_api_pb2_grpc
 import trillian_log_api_pb2
+import hashlib
+import logging
+import time
 
 
 
@@ -24,7 +27,8 @@ class AuthorizationServer:
                                             BestAvailableEncryption(password=b'password'))
 
         self.pk = key_pair[1].public_bytes(encoding=serialization.Encoding.PEM,
-                                            format=serialization.PublicFormat.SubjectPublicKeyInfo)
+                                            format=serialization.PublicFormat.
+                                            SubjectPublicKeyInfo)
         self.issuer_uri = issuer_uri
         self.id = 0
 
@@ -49,7 +53,7 @@ class AuthorizationServer:
 
 
 class Trillian:
-    def __init__(self, key_pair, key_algorithm=['ES256','RS256'], allowed_servers=[]):
+    def __init__(self, key_pair, key_algorithm=['ES256','RS256'], allowed_servers={}):
         '''Initialize the trillian personality with its key pair,
         the allowed algorithms and the dictionaries of the resource
         servers and clients that are added to the Merkle Tree. Start
@@ -60,7 +64,8 @@ class Trillian:
                                             BestAvailableEncryption(password=b'password2'))
 
         self.pk = key_pair[1].public_bytes(encoding=serialization.Encoding.PEM,
-                                            format=serialization.PublicFormat.SubjectPublicKeyInfo)
+                                            format=serialization.PublicFormat.
+                                            SubjectPublicKeyInfo)
         self.allowed_algorithms = key_algorithm
         self.allowed_servers = allowed_servers
         self.server = {}
@@ -72,6 +77,7 @@ class Trillian:
         #Create the three and initialize it
         self.tree_id = self.create_tree()
         self.init_log(self.tree_id)
+        self.tree_size = 0
 
 
     @classmethod
@@ -83,103 +89,146 @@ class Trillian:
 
     def create_tree(self):
         '''Create the tree'''
-        time = duration_pb2.Duration(seconds=3600)
-        response = self.stub.CreateTree(trillian_admin_api_pb2. \
-        CreateTreeRequest(tree={"tree_state":'ACTIVE',"tree_type":'LOG', \
-        "hash_strategy":'RFC6962_SHA256', "hash_algorithm":'SHA256', \
-        "signature_algorithm":'ECDSA', "display_name":'', \
-        "description":'',"max_root_duration":time}, \
-        key_spec=crypto.keyspb.keyspb_pb2.Specification \
-        (ecdsa_params={"curve":'P256'})))
+        max_root_duration = duration_pb2.Duration(seconds=3600)
+        response = self.stub.CreateTree(
+            trillian_admin_api_pb2.CreateTreeRequest(tree={"tree_state":'ACTIVE',
+                                                           "tree_type":'LOG',
+                                                           "hash_strategy":'RFC6962_SHA256',
+                                                           "hash_algorithm":'SHA256', 
+                                                           "signature_algorithm":'ECDSA', 
+                                                           "display_name":'', 
+                                                           "description":'',
+                                                           "max_root_duration":max_root_duration}, 
+                                                     key_spec=crypto.keyspb.keyspb_pb2.
+                                                     Specification(
+                                                         ecdsa_params={"curve":'P256'})))
         return response.tree_id
 
 
     def init_log(self,tree_id):
         '''Log to initialize the tree'''
-        response = self.stub2.InitLog(trillian_log_api_pb2. \
-        InitLogRequest(log_id=tree_id, \
-        charge_to=trillian_log_api_pb2.ChargeTo(user='test')))
+        response = self.stub2.InitLog(trillian_log_api_pb2.
+                                      InitLogRequest(log_id=tree_id, 
+                                                     charge_to=trillian_log_api_pb2.
+                                                     ChargeTo(user='test')))
         return response
 
 
-    def queue_leaf(self,tree_id,leaf_value,extra_data=''):
+    def queue_leaf(self,tree_id,leaf_value,leaf_identity_hash,extra_data=''):
         '''Insert a leaf in the tree'''
-        response = self.stub2.QueueLeaf(trillian_log_api_pb2. \
-        QueueLeafRequest(log_id=tree_id, \
-        leaf={'leaf_value':f'{leaf_value}'.encode(),'extra_data':f'{extra_data}'.encode()}))
+        response = self.stub2.QueueLeaf(
+            trillian_log_api_pb2.
+            QueueLeafRequest(log_id=tree_id, 
+                             leaf={'leaf_value':f'{leaf_value}'.encode(),
+                                   'extra_data':f'{extra_data}'.encode(),
+                                   'leaf_identity_hash':leaf_identity_hash}))
         return response
 
 
-    def inclusion_proof(self,tree_id,leaf_id):
+    def inclusion_proof(self,tree_id,leaf_id,tree_size):
         '''Return the inclusion proof for a leaf, indicated with his id,
         for a specific size of the tree'''
-        response = self.stub2.GetInclusionProof(trillian_log_api_pb2. \
-        GetInclusionProofRequest(log_id=tree_id, \
-        leaf_index=leaf_id, tree_size=(leaf_id+1)))
+        response = self.stub2.GetInclusionProof(
+            trillian_log_api_pb2.GetInclusionProofRequest(log_id=tree_id, 
+                                                          leaf_index=leaf_id, 
+                                                          tree_size=tree_size))
+        return response
+    
+    def inclusion_proof_by_hash(self, tree_id, merkle_leaf_hash, tree_size):
+        '''Return the inclusion proof for a leaf, indicated with his hash,
+        for a specific size of the tree'''
+        #m = hashlib.sha256()
+        #m.update(f'\0{leaf_value}'.encode())
+        #leaf_hash = bytes.fromhex(m.digest().hex())
+        response = self.stub2.GetInclusionProofByHash(
+            trillian_log_api_pb2.GetInclusionProofByHashRequest(log_id=tree_id,
+                                                                leaf_hash=merkle_leaf_hash, 
+                                                                tree_size=tree_size))
         return response
 
 
-    def update_info(self,data):
+    def update_info(self,data,leaf_index):
         '''Update the dictionary of clients and servers that are added
         to the log'''
         if data["server"] in self.server.keys():
-            self.server[data["server"]].append([data["client"], data["id"],
+            self.server[data["server"]].append([data["client"], leaf_index,
                                                 data["nbf"], data["exp"]])
         else:
-            self.server[data["server"]] = [[data["client"], data["id"],
+            self.server[data["server"]] = [[data["client"], leaf_index,
                                             data["nbf"], data["exp"]]]
         if data["client"] in self.clients.keys():
-            self.clients[data["client"]].append([data["server"], data["id"],
+            self.clients[data["client"]].append([data["server"], leaf_index,
                                                  data["nbf"], data["exp"]])
         else:
-            self.clients[data["client"]] = [[data["server"], data["id"],
+            self.clients[data["client"]] = [[data["server"], leaf_index,
                                              data["nbf"], data["exp"]]]
 
 
     def decode_jwt(self, token, authorization_server_pk):
         '''Decode a JWT'''
         try:
-            data = jwt.decode(jwt=token, key=authorization_server_pk, issuer=self.allowed_servers,
-                              algorithms=self.allowed_algorithms, options={"require_exp":True,
-                                                                           "verify_exp":True, 
-                                                                          # "verify_iss":True,
-                                                                           "require_nbf":True})
-            if data["issuer"] not in self.allowed_servers:
-                print("Invalid issuer")
-                return
-            return data
+            data = jwt.decode(jwt=token, key=authorization_server_pk,
+                              algorithms=self.allowed_algorithms, 
+                              options={"require_exp":True,"verify_exp":True,
+                                       # "verify_iss":True,
+                                       "require_nbf":True})
 #        except jwt.exceptions.InvalidIssuerError:
 #            print("Invalid issuer")
 #            return
-        except jwt.exceptions.ExpiredSignatureError:
+        except jwt.exceptions.ExpiredSignatureError as e:
             print("The token has expired")
-            return
-        except jwt.exceptions.ImmatureSignatureError:
+            logging.exception(e)
+            raise ValueError("Expired Token") from e
+        except jwt.exceptions.ImmatureSignatureError as e:
             print("The validity of the token has not started")
-        except jwt.exceptions.InvalidTokenError:
+            logging.exception(e)
+            raise ValueError("Immature Token") from e
+        except jwt.exceptions.InvalidTokenError as e:
             print("Invalid token")
-            return
+            logging.exception(e)
+            raise ValueError("Invalid Token") from e
+        if data["iss"] not in self.allowed_servers.keys() or \
+            self.allowed_servers[data["iss"]] != \
+                authorization_server_pk:
+            print("Invalid issuer")
+            raise ValueError("Invalid Issuer")
+        return data
 
 
-    def insert_jwt(self, token, authorization_server):
-        data = self.decode_jwt(token, authorization_server.pk)
+    def insert_jwt(self, token, authorization_server_pk):
+        data = self.decode_jwt(token, authorization_server_pk)
         print(data)
-        if data != None:
+        if data is not None:
             #leaf_value = {'client':data["client"], 'server':data["server"]}
             #leaf_value_serialize = list(sorted(leaf_value.items()))
-            leaf_value = json.dumps({'client':data["client"], 'server':data["server"]},
-                                    sort_keys=True)
-            leaf_id = data["id"]
+            leaf_value = json.dumps({'client':data["client"], 
+                                     'server':data["server"], 'id':data["id"], 
+                                     'exp':data["exp"], 'nbf':data["nbf"]},  
+                                     sort_keys=True)
+            leaf_identity = f"{data['client']} {data['server']}"
+            m = hashlib.sha256()
+            m.update(f'{leaf_identity}'.encode())
+            leaf_identity_hash = m.digest()
             print(f'Leaf value: {leaf_value}')
-            leaf_added = self.queue_leaf(self.tree_id, leaf_value)
+            leaf_added = self.queue_leaf(self.tree_id, leaf_value, leaf_identity_hash)
             print(f'Leaf added:\n{leaf_added}')
-            if leaf_added.queued_leaf.leaf.leaf_index != -1:
-                self.update_info(data)
+            time.sleep(1)     #Waiting to be sure that the leaf has been added
+            inclusion = leaf_added.queued_leaf.leaf.leaf_index
+            merkle_leaf_hash = leaf_added.queued_leaf.leaf.merkle_leaf_hash
+            #print(f'Index: {leaf_index}\n Hash:{merkle_leaf_hash}')
+            if inclusion != -1:
+                self.tree_size += 1
+                inclusion_proof = self.inclusion_proof_by_hash(self.tree_id, 
+                                                               merkle_leaf_hash, 
+                                                               self.tree_size)
+                print(f'Inclusion Proof:\n{inclusion_proof}')
+                leaf_index = inclusion_proof.proof[0].leaf_index
+                self.update_info(data, leaf_index)
             else:
                 print("Leaf already present in the log")
-                authorization_server.id-=1
-            inclusion_proof = self.inclusion_proof(self.tree_id,leaf_id)
-            print(f'Inclusion Proof:\n{inclusion_proof}')
+                self.verify_inclusion(data["client"], data["server"])
+            #leaf_index = inclusion_proof.proof.leaf_index
+            
 
 
     def check_server(self, server):
@@ -187,8 +236,10 @@ class Trillian:
         that are allowed to access it'''
         try:
             return self.server[f"{server}"]
-        except KeyError:
+        except KeyError as e:
             print("The server is not present in the log")
+            logging.exception(e)
+            raise KeyError("The server is not present in the log") from e
 
 
     def check_client(self, client):
@@ -196,5 +247,23 @@ class Trillian:
         that he has permission to access'''
         try:
             return self.clients[f"{client}"]
-        except:
+        except KeyError as e:
             print("The client is not present in the log")
+            logging.exception(e)
+            raise KeyError("The client is not present in the log") from e
+            
+    
+    def verify_inclusion(self, client, resource_server):
+        server_connected = self.check_client(client)
+        #[server, id, start_validity, expiration_time]
+        servers = [server for server in server_connected 
+                  if server[0] == resource_server]
+        if servers is not None:
+            for server in servers:
+                if datetime.fromtimestamp(server[3]) >= datetime.utcnow():
+                    leaf_id = server[1]
+                    print(f"Inclusion proof:\n"
+                          f"{self.inclusion_proof(self.tree_id, leaf_id, self.tree_size)}")
+        else:
+            print("The client is not allowed in the server")
+        
